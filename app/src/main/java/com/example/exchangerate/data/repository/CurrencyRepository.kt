63a28ced481e73754fd9067ed793bacf4f967779
Muhammad Nizwa <muhammadnizwa@gmail.com>
2022-10-07 00:24:45 +0700

    fix crash

--- app/src/main/java/com/example/exchangerate/data/repository/CurrencyRepository.kt
+++ app/src/main/java/com/example/exchangerate/data/repository/CurrencyRepository.kt
@@ -1,5 +1,6 @@
 package com.example.exchangerate.data.repository
 
+import android.util.Log
 import com.example.exchangerate.data.datasourceimpls.cache.CurrencyCache
 import com.example.exchangerate.data.datasourceimpls.database.dao.CurrencyDao
 import com.example.exchangerate.data.datasourceimpls.database.model.CurrencyNameEntity
@@ -11,8 +12,7 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class CurrencyRepository @Inject constructor(
-    private val currencyRetrofit: CurrencyRetrofit,
-    private val currencyDao: CurrencyDao
+    private val currencyRetrofit: CurrencyRetrofit, private val currencyDao: CurrencyDao
 ) {
     suspend fun syncCurrencyData() {
         if (SyncUtil.isCurrencyDataAvailableToSync(CurrencyCache.lastSyncTimeStamp?.toLong())) {
@@ -26,30 +26,36 @@ class CurrencyRepository @Inject constructor(
     }
 
     private suspend fun syncCurrencyNamesData() {
-        val networkResult = currencyRetrofit.getCurrencyNames()
+        try {
+            val networkResult = currencyRetrofit.getCurrencyNames()
 
-        currencyDao.deleteCurrencyNamesTable()
+            currencyDao.deleteCurrencyNamesTable()
 
-        currencyDao.insertAllCurrencyNames(networkResult.map {
-            CurrencyNameEntity(
-                id = it.key,
-                name = it.value
-            )
-        })
+            currencyDao.insertAllCurrencyNames(networkResult.map {
+                CurrencyNameEntity(
+                    id = it.key, name = it.value
+                )
+            })
+        } catch (e: Exception) {
+            Log.e(null, e.message, e.cause)
+        }
     }
 
     private suspend fun syncCurrencyRatesData() {
-        val networkResult = currencyRetrofit.getCurrencyRates()
+        try {
+            val networkResult = currencyRetrofit.getCurrencyRates()
 
-        currencyDao.deleteCurrencyRatesTable()
+            currencyDao.deleteCurrencyRatesTable()
 
-        currencyDao.insertAllCurrencyRates(networkResult.rates.map {
-            CurrencyRateEntity(
-                id = it.key,
-                rateVsUsd = it.value
-            )
-        })
+            currencyDao.insertAllCurrencyRates(networkResult.rates.map {
+                CurrencyRateEntity(
+                    id = it.key, rateVsUsd = it.value
+                )
+            })
 
-        CurrencyCache.updateLastSyncTimeStamp(networkResult.timestamp)
+            CurrencyCache.updateLastSyncTimeStamp(networkResult.timestamp)
+        } catch (e: Exception) {
+            Log.e(null, e.message, e.cause)
+        }
     }
 }
